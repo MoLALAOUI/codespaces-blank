d76861605dd625a1a6b3c3dd32d32427d0579561
MoLALAOUI <130655956+MoLALAOUI@users.noreply.github.com>
2023-04-13 10:29:14 +0000

    first commit

---
+++ testCopilot.java
@@ -0,0 +1,14 @@
+public class testCopilot {
+    //function to compare date
+public String compareInt(int a,int b) {
+    String result = "TRUE";
+    if(a > b) {
+        result = "A Plus grand que B";
+    } else {
+        result = "A Plus petit que B";
+    }
+return result;
+}
+
+}
+
